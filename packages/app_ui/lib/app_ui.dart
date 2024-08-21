95e830876fde05e3ed80774117737449cda188f5
kitety <aizaizuori@foxmail.com>
2024-08-21 12:04:08 +0800

    13 Theme and styles extensions

--- packages/app_ui/lib/app_ui.dart
+++ packages/app_ui/lib/app_ui.dart
@@ -4,6 +4,7 @@ library;
 export 'package:flex_color_scheme/flex_color_scheme.dart';
 
 export 'src/colors/app_colors.dart';
+export 'src/extensions/extensions.dart';
 export 'src/generated/generated.dart';
 export 'src/spacing/app_spacing.dart';
 export 'src/theme/app_theme.dart';
